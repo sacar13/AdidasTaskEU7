08e0476e5086dde25f68739227f552bab340a8cf
OscarSah <oscar@cybertekschool.com>
2022-02-06 18:46:41 +0300

    Creating Step Definitions

---
+++ src/test/java/com/demoblaze/step_definitions/PurchaseSteps.java
@@ -0,0 +1,17 @@
+package com.demoblaze.step_definitions;
+
+import com.demoblaze.utilities.ConfigurationReader;
+import com.demoblaze.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class PurchaseSteps {
+    @Given("User is on the home page")
+    public void user_is_on_the_home_page() {
+        Driver.get().get(ConfigurationReader.get("demoblaze.url"));
+    }
+    @When("User adds {string} from {string}")
+    public void user_adds_from(String product, String category) {
+
+    }
+}
