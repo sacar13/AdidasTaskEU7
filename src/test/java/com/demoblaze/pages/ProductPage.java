7447eab3b94cea4c5054ca4bb06240225f49bc23
OscarSah <oscar@cybertekschool.com>
2022-02-06 19:10:10 +0300

    Creating POMs

--- src/test/java/com/demoblaze/pages/ProductPage.java
+++ src/test/java/com/demoblaze/pages/ProductPage.java
@@ -1,14 +1,17 @@
 package com.demoblaze.pages;
 
 import com.demoblaze.utilities.Driver;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ProductPage extends BasePage{
 
     @FindBy(xpath = "//a[.='Add to cart']")
-    public WebElement addToCart;
+    public WebElement addToCartElement;
 
     public void navigateTo(String product, String category){
         Driver.get().findElement(By.linkText(category)).click();
@@ -16,7 +19,11 @@ public class ProductPage extends BasePage{
     }
 
     public void addToCart(){
-        addToCart.click();
+        addToCartElement.click();
+        WebDriverWait wait = new WebDriverWait(Driver.get(),10);
+        Alert alert = Driver.get().switchTo().alert();
+        wait.until(ExpectedConditions.alertIsPresent());
+        alert.accept();
     }
 
 }
