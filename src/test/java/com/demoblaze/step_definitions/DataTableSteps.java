2818747ec2aa658aceb8ed47376615ba8504f5e0
OscarSah <oscar@cybertekschool.com>
2022-02-13 18:21:27 +0300

    Data Tables step is created

--- src/test/java/com/demoblaze/step_definitions/DataTableSteps.java
+++ src/test/java/com/demoblaze/step_definitions/DataTableSteps.java
@@ -1,8 +1,10 @@
 package com.demoblaze.step_definitions;
 
 import com.demoblaze.pages.ProductPage;
+import com.demoblaze.utilities.BrowserUtils;
 import com.demoblaze.utilities.Driver;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -16,7 +18,11 @@ public class DataTableSteps {
     @Then("Under {string} category User should be to see the list of following products")
     public void under_category_user_should_be_to_see_the_list_of_following_products(String category, List<String> expectedProducts) {
         Driver.get().findElement(By.linkText(category)).click();
-        List<WebElement> productsElements = page.products;
-        List<String>
+        BrowserUtils.sleep(2);
+
+        List<String> actualProducts = BrowserUtils.getElementsText(page.products);
+        System.out.println("expectedProducts = " + expectedProducts);
+        System.out.println("actualProducts = " + actualProducts);
+        Assert.assertEquals("List of Products are NOT as expected",expectedProducts,actualProducts);
     }
 }
